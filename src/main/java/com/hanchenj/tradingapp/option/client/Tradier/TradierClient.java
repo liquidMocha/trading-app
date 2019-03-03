51194500d39f80bb11a78d22f6ce0bb565b1a95a
Hanchen Jiang <hanchenjhc@gmail.com>
2019-03-03 11:23:24 -0600

    Use the next Friday as expiration date [#164193578]

--- src/main/java/com/hanchenj/tradingapp/option/client/Tradier/TradierClient.java
+++ src/main/java/com/hanchenj/tradingapp/option/client/Tradier/TradierClient.java
@@ -2,17 +2,22 @@ package com.hanchenj.tradingapp.option.client.Tradier;
 
 import com.hanchenj.tradingapp.option.client.OptionInfoClient;
 import com.hanchenj.tradingapp.option.domain.Option;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.*;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAdjusters;
 import java.util.Collections;
 import java.util.List;
 
 @Component
+@Slf4j
 public class TradierClient implements OptionInfoClient {
     private RestTemplate restTemplate;
 
@@ -26,8 +31,7 @@ public class TradierClient implements OptionInfoClient {
 
         UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl("https://sandbox.tradier.com/v1/markets/options/chains")
                 .queryParam("symbol", ticker)
-//                .queryParam("expiration", DateTimeFormatter.ISO_LOCAL_DATE.format(today));
-                .queryParam("expiration", "2019-03-01");
+                .queryParam("expiration", getNextFriday(today));
 
         HttpHeaders headers = new HttpHeaders();
         headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
@@ -43,5 +47,9 @@ public class TradierClient implements OptionInfoClient {
         TradierOptionChainResponse response = responseEntity.getBody();
 
         return response.transformToOptions();
+    }
+
+    private String getNextFriday(LocalDate today) {
+        return DateTimeFormatter.ISO_LOCAL_DATE.format(today.with(TemporalAdjusters.next(DayOfWeek.FRIDAY)));
     }
 }
--- src/main/java/com/hanchenj/tradingapp/option/client/Tradier/TradierOptionChainResponse.java
+++ src/main/java/com/hanchenj/tradingapp/option/client/Tradier/TradierOptionChainResponse.java
@@ -5,11 +5,17 @@ import lombok.Data;
 
 import java.util.List;
 
+import static java.util.Collections.emptyList;
+
 @Data
 class TradierOptionChainResponse {
     private Options options;
 
     List<com.hanchenj.tradingapp.option.domain.Option> transformToOptions() {
-        return this.options.transformToDomainOptions();
+        if (this.options != null) {
+            return this.options.transformToDomainOptions();
+        }
+
+        return emptyList();
     }
 }
